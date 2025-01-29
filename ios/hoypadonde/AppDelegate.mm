9ddeb0dd70ed19a05223ac9d908c722455be03f2
José Luis Cueto Simancas <jcuetos@aeromexico.com>
2025-01-29 15:29:07 -0700

    remove firebase from proyect

--- ios/hoypadonde/AppDelegate.mm
+++ ios/hoypadonde/AppDelegate.mm
@@ -1,13 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <Firebase.h>
+// #import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
+  // [FIRApp configure];
   self.moduleName = @"hoypadonde";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
